4cf776a3d5a5de78f1ba49b05fd685f45f209325
Mike Harvey <mharvey25@gmail.com>
2018-12-19 21:53:25 -0700

    more progress

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friendsArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            5,
+            1,
+            5,
+            4,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            5,
+            4,
+            1,
+            1,
+            4,
+            4,
+            5,
+            1,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            5,
+            4,
+            1,
+            4,
+            4,
+            5,
+            1,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            5,
+            5,
+            4,
+            1,
+            5,
+            4,
+            3
+        ]
+    },
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
